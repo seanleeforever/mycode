432b3d3d63440e1c3f2d87e45d427d46ce05509e
seanleeforever <sean.li@live.com>
2023-05-15 22:19:04 +0000

    introduction to regex searching with python

---
+++ demo3.py
@@ -0,0 +1,8 @@
+contact = { "phone": "1234567",
+            "port" : 5060,
+            "ipv6" : "2600:2304:9210:ec::2"}
+
+phonenum = contact ["phone"]
+portnum = contact ["port"]
+
+print (phonenum, portnum)
